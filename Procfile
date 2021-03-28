655d83c999b3ede64dee841b036f63d614e43e4a
gael <cgvalenzuelac@concredito.com.mx>
2021-03-28 00:17:29 -0700

    requerimientos.

--- Procfile
+++ Procfile
@@ -1,1 +1,1 @@
-web: gunicorn --bind 0.0.0.0:$PORT config.wsgi:app
+web: gunicorn --bind 0.0.0.0:$PORT config.wsgi:app  --timeout 10
--- models/UserDoc.py
+++ models/UserDoc.py
@@ -6,7 +6,7 @@ class UserDoc(Document):
     name = StringField(required=True, max_length=20)
     lastName = StringField(required=True, max_length=30)
     email = EmailField(required=True, max_length=50, unique=True)
-    date_modified = DateTimeField(default=datetime.datetime.utcnow)
+    date_modified = DateTimeField(default=datetime.datetime.now)
 
     def parseJson(self):
         if self.id is not None:
