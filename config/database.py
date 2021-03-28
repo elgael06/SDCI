ca61ee6d8cdc26bd0f9fa825ba49e50152d002c6
gael <cgvalenzuelac@concredito.com.mx>
2021-03-28 00:39:55 -0700

    change host mongo.

--- config/database.py
+++ config/database.py
@@ -2,8 +2,9 @@
 from mongoengine import connect
 
 DB = 'base'
-HOST = 'mongodb+srv://admin:1234@cluster0.4geja.mongodb.net/'
+HOST = 'mongodb+srv://admin:1234@cluster0.4geja.mongodb.net/base'
+HOST_2 = 'mongodb+srv://admin:1234@cluster0.4geja.mongodb.net/base?retryWrites=true&w=majority'
 LOCALHOST = "mongodb://127.0.0.1:27017/"
 PORT = 27017
 # Conexion base
-connect(host=HOST+DB)
+connect(host=HOST_2)
