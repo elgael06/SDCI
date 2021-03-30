918a50b695b830547110ae1ec33e93fb940f1cd1
gael <cgvalenzuelac@concredito.com.mx>
2021-03-29 21:17:25 -0700

    estructuracion de configuraciones de bd.

--- app.py
+++ app.py
@@ -1,24 +1,23 @@
 import connexion
-import config.database
+import config.database.mongo_bd
 from flask_cors import CORS
-###
-# se cargan los archivos de rutas
+
 ##
-# rutas de api base para usuarios
+# instancia de la app con la rutas de configuracion de las api.
+##
 app = connexion.App(
-    __name__, specification_dir='./config/api/')
+    __name__, specification_dir='./config/api/', options={"swagger_ui": False})
 
 ###
 # se agregan las rutas al app
 ##
-app.add_api('user_v1.yml')
-app.add_api('sesion_v1.yml')
-
-CORS(app.app)
+app.add_api('user_v1.yml')  # api v1 usuarios
+app.add_api('sesion_v1.yml')  # api v1 sesion
+CORS(app.app)   # activacion de cors
 
 
 @app.route('/')
-def hello_world():
+def index():
     return 'Este el el index de sistema base para aplicaciones.'
 
 
