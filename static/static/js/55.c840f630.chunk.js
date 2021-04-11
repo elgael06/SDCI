ee068963b57382ab0d27d1cf480091a093cc1a48
gael <cgvalenzuelac@concredito.com.mx>
2021-04-11 04:35:55 -0600

    new build.

--- static/static/js/55.c840f630.chunk.js
+++
@@ -1,2 +0,0 @@
-(this["webpackJsonpfront-lamar"]=this["webpackJsonpfront-lamar"]||[]).push([[55],{222:function(n,t,o){"use strict";o.r(t),o.d(t,"startStatusTap",(function(){return r}));var e=o(23),r=function(){var n=window;n.addEventListener("statusTap",(function(){Object(e.h)((function(){var t=n.innerWidth,o=n.innerHeight,r=document.elementFromPoint(t/2,o/2);if(r){var i=r.closest("ion-content");i&&i.componentOnReady().then((function(){Object(e.f)((function(){return i.scrollToTop(300)}))}))}}))}))}}}]);
-//# sourceMappingURL=55.c840f630.chunk.js.map
